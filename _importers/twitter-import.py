b5ed9d960c4f6dc4808b1ff6910765ed5f979e32
Richard Saunders <r.saunders@zengenti.com>
2019-11-28 16:28:46 +0000

    Added tweets

--- _importers/twitter-import.py
+++ _importers/twitter-import.py
@@ -1,133 +1,134 @@
-import codecs
 from datetime import datetime
 import json
 import os
 import posixpath
-import re
-import shutil
 import sys
+import re
 
-# This script will import your instagram photos to your static site. It should work for most, assuming you customize the POST_TEMPLATE to match what your site uses.
-# Things to note:
-#   * multi-photo posts will be imported as standalone photos with no caption (that's how Instagram exports them)
-#   * hashtags get removed from posts, unless you set REMOVE_HASH_TAGS to False.
-#   * multiline instagram posts are not handled gracefully if you're using the default POST_TEMPLATE (instagram image caption doesn't work with new lines)
+# Path to store tweets
+tweet_path = "_tweets"
 
-# Configuration parameters START
+# -------------------------------------------
+# Tweet filenames based on original tweet ID
+# -------------------------------------------
+tweet_file_template = "{tweet_id}.md"
 
-# All of these paths will be strftime formatted with capture time
-#BLOG_PATH = "content/blog/%Y/%m"
-BLOG_PATH = "_tweets"
-#MEDIA_PATH = "content/media/images/photos/%Y/%m"
-MEDIA_PATH = "assets/uploads/%Y/%m"
-#MEDIA_URL = "/media/images/photos/%Y/%m"
-MEDIA_URL = "assets/uploads/%Y/%m"
-
-# BLOG_POST_FILE_TEMPLATE will be formatted with parameters: capture_time, stub
-BLOG_POST_FILE_TEMPLATE = "{capture_time:%Y}-{capture_time:%m}-{capture_time:%d}-{slug:s}.md"
-
-# POST_TEMPLATE will be formatted with post_time, caption, and photo_file parameters. Photo file is full path derived from MEDIA_URL
-#POST_TEMPLATE = """---
-#type: photo
-#created: !!timestamp '{post_time:%Y-%m-%d %H:%M:%S}'
-#tags:
-#    - microblog
-#---
-#![{caption:s}]({photo_path:s})
-#
-#"""
-
-POST_TEMPLATE = """---
-title: "{caption:s}"
-caption: "{caption:s}"
-date: {post_time:%Y-%m-%d %H:%M}
-location: "{location}"
-tags: "{tags:s}"
-image: {photo_path:s}
+# -------------------------------------------
+# Markdown template
+# -------------------------------------------
+tweet_yml_template = """---
+title: "{tweet_id}"
+date: {date}
+original-tweet-id: "{tweet_id}"
+retweets: {retweet_count}
+favorites: {favorite_count}
+reply-tweet-id: "{reply_tweet_id}"
+reply-user-id: "{reply_user_id}"
+reply-user-screen-name: "{reply_user_screen_name}"
 ---
+{tweet}
 """
 
-REMOVE_HASH_TAGS = True
-
-# Configuration parameters END
-
 if len(sys.argv) < 2:
-    print 'First parameter has to be path to directory with Instagram data'
+    print ("First parameter has to be path to directory with tweet data")
     exit(1)
 
-instagram_data = sys.argv[1]
-media_file = os.path.join(instagram_data, 'media.json')
+tweet_data = sys.argv[1]
+twitter_export_file = os.path.join(tweet_data, 'tweet.json')
 data = {}
 
-if not os.path.exists(instagram_data) or not os.path.exists(media_file):
-    print """Directory "{:s}" doesn't exist or is missing media.json file"""
+if not os.path.exists(tweet_data) or not os.path.exists(twitter_export_file):
+    print ("Directory does not exist or is missing tweet.json file")
     exit(1)
 
-with open(media_file) as fd:
+# -------------------------------------------
+# Load JSON
+# -------------------------------------------
+with open(twitter_export_file) as fd:
     data = json.load(fd)
 
-photos = data.get('photos', [])
+for tweet in data:
 
-for photo in photos:
+    # -------------------------------------------
+    # Create variables of the tweet data
+    # -------------------------------------------
 
-    if REMOVE_HASH_TAGS:
-        caption = re.sub(r'#[^\n\s]+', '', photo['caption']).strip()
+    # Tweet
+    full_text = tweet['full_text'].encode('utf-8').strip()
+    created_at = tweet['created_at']
+    id_str = tweet['id_str']
 
-    # Encode everything as UTF-8
-    caption = codecs.encode(caption, 'utf-8')
-    capture_time = datetime.strptime(photo['taken_at'], "%Y-%m-%dT%H:%M:%S")
-    instagram_photo_path = os.path.join(instagram_data, photo['path'])
-    location = codecs.encode(photo.get('location', ''),'utf-8')
-    tags = re.findall(r'#[^\n\s]+', photo['caption'])
-    taglist = [tag.encode('utf-8') for tag in tags]
-    cleantaglist = map(lambda each:each.strip("#"), taglist)
-    flatlist = ' '.join(map(str, cleantaglist)) 
+    # Tweet metadata
+    favorite_count = tweet['favorite_count']
+    retweet_count = tweet['retweet_count']
 
-    # If the image file is not there, move on. If you don't want to import certain images, just delete them first.
-    if not os.path.exists(instagram_photo_path):
-        continue
+    # Replies
+    reply_tweet_id = tweet.get('in_reply_to_status_id_str', '').encode('utf-8').strip()
+    reply_user_id = tweet.get('in_reply_to_user_id_str', '').encode('utf-8').strip()
+    reply_user_screen_name = tweet.get('in_reply_to_screen_name', '').encode('utf-8').strip()
+    
+    #print(reply_user_screen_name)
 
-    blog_path = datetime.strftime(capture_time, BLOG_PATH)
-    media_path = datetime.strftime(capture_time, MEDIA_PATH)
-    media_url = datetime.strftime(capture_time, MEDIA_URL)
+    # --------------------------------------------------
+    # @todo: Convert twitter shortened URLs to originals
+    # --------------------------------------------------
 
-    photo_file = os.path.basename(instagram_photo_path)
 
-    post = POST_TEMPLATE.format(
-        post_time=capture_time,
-        caption=caption,
-        photo_path=posixpath.join(media_url, photo_file),
-        location=location,
-        tags= flatlist
+    # -------------------------------------------
+    # Look for any URLs and convert to HTML links
+    # -------------------------------------------
+    _link = re.compile(r'(?:(https://)|(www\.))(\S+\b/?)([!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~]*)(\s|$)', re.I)
+
+    def convertLinks(text): 
+        def replace(match):
+            groups = match.groups()
+            protocol = groups[0] or ''  # may be None
+            www_lead = groups[1] or ''  # may be None
+            return '<a href="https://{1}{2}">{0}{1}{2}</a>{3}{4}'.format(
+                protocol, www_lead, *groups[2:])
+        return _link.sub(replace, text)
+
+    full_text_linked = convertLinks(full_text)
+
+    # ----------------------------------------------------------
+    # Look for any @mentions and convert to twitter profile links
+    # ----------------------------------------------------------
+
+    _mention = re.compile(r'@([^:\s]+)', re.I)
+
+    def convertMentions(text): 
+        def replace(match):
+            groups = match.groups()
+            return '<a href="https://twitter.com/{0}">@{0}</a>'.format(*groups[0:])
+        return _mention.sub(replace, text)
+
+    full_text_mention_links = convertMentions(full_text_linked)
+
+    # -------------------------------------------
+    # Setup the YAML Template
+    # -------------------------------------------
+    post = tweet_yml_template.format (
+        tweet = full_text_mention_links,
+        date = created_at,
+        tweet_id = id_str,
+        favorite_count = favorite_count,
+        retweet_count = retweet_count,
+        reply_tweet_id = reply_tweet_id,
+        reply_user_id = reply_user_id,
+        reply_user_screen_name = reply_user_screen_name
     )
 
-    post_file = None
+    tweet_file = tweet_file_template.format (
+        tweet_id = id_str
+    )
 
-    if caption:
-        slug = re.sub(r"[^a-z0-9 ]", '', caption.lower()).strip()
-        slug = re.sub(r" (the|a|an) ", ' ', slug).strip()
-        slug = re.sub(r"\s+", ' ', slug).strip()
-        slug = "-".join(slug.split()[0:3])
-        
-        post_file = BLOG_POST_FILE_TEMPLATE.format(
-            capture_time=capture_time,
-            slug=slug
-        )
-
-        # If the filename derived from caption exists, don't use it
-        if os.path.exists(os.path.join(blog_path, post_file)):
-            post_file = None
-
-    if not post_file:
-        post_file = BLOG_POST_FILE_TEMPLATE.format(
-            capture_time=capture_time,
-            slug="instagram-{:s}".format(os.path.splitext(photo_file)[0])
-        )
+    # -------------------------------------------
+    # Write files to path specified in post_path
+    # -------------------------------------------
     
-    post_path = os.path.join(blog_path, post_file)
+    post_path = os.path.join(tweet_path, tweet_file)
 
     try:
-        os.makedirs(media_path)
         os.makedirs(os.path.dirname(post_path))
     except OSError as err:
         # Ignore "directory exists" errors
@@ -136,4 +137,3 @@ for photo in photos:
 
     with open(post_path, 'w') as fd:
         fd.write(post)
-        shutil.copy(instagram_photo_path, media_path)
